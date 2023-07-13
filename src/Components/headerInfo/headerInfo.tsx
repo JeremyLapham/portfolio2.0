94777a9e1036a372935d234766d818b63c3748e5
JeremyLapham <jeremylapham2004@gmail.com>
2023-07-13 08:38:09 -0700

    portfolio 2.0

---
+++ src/Components/headerInfo/headerInfo.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function headerInfo() {
+  return (
+    <div>headerInfo</div>
+  )
+}
---
+++ src/Components/skillsCard/skillsCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function skillsCard() {
+  return (
+    <div>skillsCard</div>
+  )
+}
